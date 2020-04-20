95453ee15c93f4565de5fcf8c3d36fbd65a722e5
iv <iv@iv.iv>
2020-04-19 21:09:15 -0400

    Reorg

---
+++ prep_c_deps.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for makefile in $(find ./c_src -name "Makefile" -exec dirname {} +); do
+    pushd "$makefile" && make && popd
+done
+
+for library in $(find ./c_src/chess -name "*.so"); do
+    name=`basename $library`
+    cp "$library" "./py_src/chess/$name"
+done
+
+
+for library in $(find ./c_src/models -name "*.so"); do
+    name=`basename $library`
+    cp "$library" "../py_src/models/$name"
+done
